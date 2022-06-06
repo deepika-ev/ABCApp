0d1d2fceffd4e15dc7b91bf5969dbe999f98f656
deepika-ev <deepikaev4444@gmail.com>
2022-06-06 02:44:56 -0700

    Initial Commit

---
+++ ABCApp/Controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ABCApp
+//
+//  Created by Shridevi Sawant on 02/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let dbUtil = DBUtility.instance
+    
+    @IBOutlet weak var btn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addClick(_ sender: Any) {
+        
+        dbUtil.addStudent(name: "", roll: 1, dob: Date() )
+    }
+    
+}
+
